2547772afa5aa644a4c33886cc6d18cc188f2940
Alex Cooper <98158236+Oraclee5321@users.noreply.github.com>
2024-10-04 11:22:12 +0100

    Success - Slow Method though at 1431ms

--- equalSkill.py
+++ equalSkill.py
@@ -1,1 +1,21 @@
-print("hello world")
+def dividePlayers(skill):
+    """
+    :type skill: List[int]
+    :rtype: int
+    """
+    skill = sorted(skill)
+    average_skill = sum(skill) / (len(skill) / 2)
+    if average_skill.is_integer():
+        return -1
+    else:
+        point_1 = 0
+        point_2 = -1
+        teams = []
+        for x in range(int(len(skill) / 2)):
+            if skill[point_1] + skill[point_2] != average_skill:
+                return -1
+            teams.append(skill.pop(point_1) * skill.pop(point_2))
+        return(sum(teams))
+
+
+print(dividePlayers([1,1,2,3]))
